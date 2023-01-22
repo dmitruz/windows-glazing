ec60540c70bf636f2ba8061cf1af14c9dfd10a95
dmitruz <dmitruz970@gmail.com>
2023-01-22 22:44:31 +0200

    feature: tabs - show content and hide content

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import "./slider";
 import modals from './modules/modals';
+import tabs from './modules/tabs';
 
 window.addEventListener('DOMContentLoaded', () => {
     modals();
+    tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
 });
