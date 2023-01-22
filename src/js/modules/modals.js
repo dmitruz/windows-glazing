005452522ddf6efa3db5c94cec2d9d69dca2e775
dmitruz <dmitruz970@gmail.com>
2023-01-22 21:54:18 +0200

    feature: madal window function

---
+++ src/js/modules/modals.js
@@ -0,0 +1,45 @@
+const modals = () => {
+    function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
+        const trigger = document.querySelectorAll(triggerSelector),
+              modal = document.querySelector(modalSelector),
+              close = document.querySelector(closeSelector),
+              windows = document.querySelectorAll('[data-modal]');
+
+        trigger.forEach(item => {
+            item.addEventListener('click', (e) => {
+                if (e.target) {
+                    e.preventDefault();
+                }
+
+                windows.forEach(item => {
+                    item.style.display = 'none';
+                });
+    
+                modal.style.display = "block";
+                document.body.style.overflow = "hidden";
+                // document.body.classList.add('modal-open');
+            });
+        });
+
+        close.addEventListener('click', () => {
+            windows.forEach(item => {
+                item.style.display = 'none';
+            });
+
+            modal.style.display = "none";
+            document.body.style.overflow = "";
+            // document.body.classList.remove('modal-open');
+        });
+
+        modal.addEventListener('click', (e) => {
+            if (e.target === modal && closeClickOverlay) {
+                windows.forEach(item => {
+                    item.style.display = 'none';
+                });
+
+                modal.style.display = "none";
+                document.body.style.overflow = ""; 
+                // document.body.classList.remove('modal-open');
+            }
+        });
+    }
