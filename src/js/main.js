395889765262fc80dfad78a5e3d2b35595cc6b9c
dmitruz <dmitruz970@gmail.com>
2023-01-22 23:06:46 +0200

    feature: send form data.

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
 import "./slider";
 import modals from './modules/modals';
 import tabs from './modules/tabs';
+import forms from './modules/forms';
 
 window.addEventListener('DOMContentLoaded', () => {
     modals();
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
+    forms();
 });
